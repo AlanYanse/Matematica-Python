2fef82e537fe94ff76aea5b27c1ed27de37d6244
Alan Yanse <yansealan@gmail.com>
2022-04-25 15:02:37 -0300

    demostración base canonica

--- demostracion-base-canonica.py
+++ demostracion-base-canonica.py
@@ -4,7 +4,12 @@ a , b = 3 , 5
 
 vector_u = [ a , b ]
 
+v_i = [1 , 0] # Unitario i
+v_j = [0 , 1] # Unitario j
+
+
 #base_canonica = a * [1 , 0] + b * [0 , 1]
-base_canonica = [ x + y for x , y in zip( [ x * a for x in [1 , 0] ],[ x * b for x in [0 , 1]  ]) ]
+#base_canonica = a * v_i + b * v_j
+base_canonica = [ x + y for x , y in zip( [ x * a for x in v_i ],[ x * b for x in v_j ]) ]
 
 print(vector_u == base_canonica)
